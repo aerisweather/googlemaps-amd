f3a476edb76fece884661aba5298054245a5c145
Edan Schwartz <eschwartz@weathernation.net>
2014-01-16 14:55:46 -0600

    Install underscore bower component

--- test/specrunner.js
+++ test/specrunner.js
@@ -2,10 +2,14 @@ require.config({
   baseUrl: '../src',
 
   paths: {
+    // Vendor libraries
     async: '../bower_components/requirejs-plugins/async',
     sinon: '../bower_components/sinon/index',
     jasmine: '../bower_components/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': '../bower_components/jasmine/lib/jasmine-core/jasmine-html',
+    underscore: '../bower_components/underscore/underscore',
+
+    // Test resources
     spec: '../test/spec',
     mock: '../test/mock'
   },
@@ -20,6 +24,9 @@ require.config({
     },
     sinon: {
       exports: 'sinon'
+    },
+    underscore: {
+      exports: '_'
     }
   }
 });
